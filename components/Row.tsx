e9ab29c629afa86ff077018915404f78076d0076
alex <gust_dev@proton.me>
2023-07-04 12:30:51 -0700

    Update Row.tsx

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,16 +1,17 @@
 interface RowProps {
   children: React.ReactNode;
   direction: string;
+  className?: string;
 }
 
-const Row = ({ children, direction }: RowProps) => {
+const Row = ({ children, direction, className = "" }: RowProps) => {
   const getDirection = (direction: string) => {
     if (direction === "right") return "lg:flex-row-reverse";
     else return "lg:flex-row";
   };
   return (
     <div
-      className={`flex flex-col ${getDirection(
+      className={`flex flex-col ${className} ${getDirection(
         direction
       )} justify-between items-center`}
     >
