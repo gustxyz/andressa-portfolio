42eed91bd9c176ab937c2fabd37edfb9998a5752
andressadalecio <140676019+andressadalecio@users.noreply.github.com>
2023-07-28 23:12:14 +0000

    change_pic_traveling

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -51,7 +51,7 @@ interface PersonalItem {
 //write a list of personal items
 const PersonalItems: PersonalItem[] = [
   {
-    imgPath: "/images/donate_test.png",
+    imgPath: "/images/traveling.jpg",
     header: "Exploring the world",
     description:
       "I’ve been traveling for 6 years now I love how there’s so much to see and learn with other cultures.",
